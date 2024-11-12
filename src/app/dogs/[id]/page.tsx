dba3cdecedf40f057312f098ee793bb2abfedc6a
ldmoore <lucasdmo@unc.edu>
2024-11-12 16:41:55 -0500

    Commit deliverable-v

--- src/app/dogs/[id]/page.tsx
+++ src/app/dogs/[id]/page.tsx
@@ -1,22 +1,45 @@
-'use client';
-import Image from "next/image";
-import { use } from "react";
-import dogInfo from "../../dogInfo.json";
+import { breeds } from "../../dogInfo.json";
 
-function adopt(): void {
-    alert("Congratulations, you adopted a dog!");
+export async function generateStaticParams() {
+    let storedDogs = [];
+
+    for (let i : number = 0; i < breeds.length; i++) {
+        const breed = breeds[i];
+        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+        const data = await response.json();
+
+        storedDogs.push({
+            id: i.toString(),
+            name: "Dog",
+            breed: breed,
+            img: data.message,
+            description: "A cute dog!"
+        });
+        console.log(storedDogs);
+    }
+
+    return storedDogs.map(dog => ({
+        params: { id: dog.breed }
+    }));
 }
 
-export default function Dog({ params }: { params: { id: string } }) {
-    //@ts-ignore
-    const { id } = use(params);
-    const dog : any = Object.values(dogInfo).find(d => d.id === id);
+export default async function Dog({ params }: { params: { id: string } }) {
+
+    const response = await fetch(`https://dog.ceo/api/breed/${params.id}/images/random`);
+    const dogResp = await response.json();
+    const dog = {
+        "id": 0,
+        "name": "Dog",
+        "breed": params.id,
+        "img": dogResp.message,
+        "description": "A cute dog!"
+    };
+
     if (!dog) return (<p>404</p>);
     else return ( <div className="dog-image">
-        <h1>Say hi to {dog.name}!</h1>
+        <h1>Say hi to this dog!</h1>
         <p>Breed: {dog.breed}<br></br>{dog.description}</p>
         <img width="50%" src={dog.img} alt={dog.breed} />
-        <button onClick={() => adopt()}>Adopt Now</button>
         <a href="/">Back to home</a>
         <br></br>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,30 @@
 import Image from "next/image";
 import DogCard from "./DogCard";
-const dogs = require("./dogInfo.json");
+const { breeds } = require("./dogInfo.json");
+
+let storedDogs: object[] = [];
+
+export async function generateStaticParams() {
+  const response = await fetch('https://dog.ceo/api/breeds/list/all');
+  const data = await response.json();
+  const breeds = Object.keys(data.message);
+
+  for (let i : number = 0; i < 5; i++) {
+    const breed = breeds[Math.floor(Math.random() * breeds.length)];
+    const response = await fetch(`https://dog.ceo/api/breed/${breed}/images/random`);
+    const data = await response.json();
+
+    storedDogs.push({
+      "id": i,
+      "name": "Dog",
+      "breed": breed,
+      "img": data.message,
+      "description": "A cute dog!"
+    });
+  }
+
+  return storedDogs;
+}
 
 export default function Home() {
   return (
@@ -8,10 +32,10 @@ export default function Home() {
       <h1>Dog Adoption Arena</h1>
       <p>Click on a dog to adopt!</p>
       <div>
-        {Object.keys(dogs).length > 0 && (
+        {breeds.length > 0 && (
           <div className="dog-list">
-          {Object.keys(dogs).map((dog : any) => (
-            <DogCard dog={dogs[dog]} key={dogs[dog].breed} />
+          {breeds.map((dog : string) => (
+            <p><a href={`/dogs/${dog}`}>{dog}</a></p>
           ))}
           </div>
         )}
